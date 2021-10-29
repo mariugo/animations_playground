81c2eb63beb106eb8c93e391ac27980e0b8e9792
Mariugo Megiani <mariugomegiani@gmail.com>
2021-10-29 18:34:18 -0300

    Initial animated container widget

--- lib/screens/animated_container_screen.dart
+++ lib/screens/animated_container_screen.dart
@@ -9,8 +9,29 @@ class AnimatedContainerScreen extends StatefulWidget {
 }
 
 class _AnimatedContainerScreenState extends State<AnimatedContainerScreen> {
+  double _width = 200;
+  double _height = 200;
+  Color _color = Colors.red;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(20);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Animated Container',
+          ),
+        ),
+        body: Center(
+          child: Container(
+            width: _width,
+            height: _height,
+            decoration:
+                BoxDecoration(color: _color, borderRadius: _borderRadius),
+          ),
+        ),
+      ),
+    );
   }
 }
