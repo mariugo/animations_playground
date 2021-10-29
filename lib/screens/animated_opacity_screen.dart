46bc86e9f209fe765e20a4c3ce7f1eb9a98ecc24
Mariugo Megiani <mariugomegiani@gmail.com>
2021-10-29 19:13:43 -0300

    Animated Opacity done

--- lib/screens/animated_opacity_screen.dart
+++ lib/screens/animated_opacity_screen.dart
@@ -1,10 +1,53 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class AnimatedOpacityScreen extends StatelessWidget {
+class AnimatedOpacityScreen extends StatefulWidget {
   const AnimatedOpacityScreen({Key? key}) : super(key: key);
 
   @override
+  State<AnimatedOpacityScreen> createState() => _AnimatedOpacityScreenState();
+}
+
+class _AnimatedOpacityScreenState extends State<AnimatedOpacityScreen> {
+  double _opacity = 1;
+
+  void _fader() {
+    setState(() {
+      _opacity = _opacity == 0 ? 1 : 0;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Animated Opacity',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedOpacity(
+              opacity: _opacity,
+              duration: const Duration(seconds: 1),
+              curve: Curves.fastOutSlowIn,
+              child: const FlutterLogo(
+                size: 200,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _fader,
+              child: const Text(
+                'Fade Logo',
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
