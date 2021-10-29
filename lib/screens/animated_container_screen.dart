ac5ec49b2dc9201d21dd8270b80ceda8088c7c90
Mariugo Megiani <mariugomegiani@gmail.com>
2021-10-29 18:45:37 -0300

    Animated Container done

--- lib/screens/animated_container_screen.dart
+++ lib/screens/animated_container_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AnimatedContainerScreen extends StatefulWidget {
@@ -13,6 +15,23 @@ class _AnimatedContainerScreenState extends State<AnimatedContainerScreen> {
   double _height = 200;
   Color _color = Colors.red;
   BorderRadiusGeometry _borderRadius = BorderRadius.circular(20);
+  final random = Random();
+
+  void _randomize() {
+    setState(() {
+      _width = random.nextInt(300).toDouble();
+      _height = random.nextInt(300).toDouble();
+
+      _color = Color.fromRGBO(
+        random.nextInt(256),
+        random.nextInt(256),
+        random.nextInt(256),
+        1,
+      );
+
+      _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble());
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,13 +43,20 @@ class _AnimatedContainerScreenState extends State<AnimatedContainerScreen> {
           ),
         ),
         body: Center(
-          child: Container(
+          child: AnimatedContainer(
+            duration: const Duration(
+              seconds: 1,
+            ),
             width: _width,
             height: _height,
             decoration:
                 BoxDecoration(color: _color, borderRadius: _borderRadius),
           ),
         ),
+        floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.play_arrow),
+            backgroundColor: Colors.green,
+            onPressed: _randomize),
       ),
     );
   }
