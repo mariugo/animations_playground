3c851fe3b1af9161dfcc7b8e5e3beb5c42b6179b
Mariugo Megiani <mariugomegiani@gmail.com>
2021-10-29 21:17:39 -0300

    Tween Animation done

--- lib/screens/tween_animation_builder_screen.dart
+++ lib/screens/tween_animation_builder_screen.dart
@@ -5,6 +5,75 @@ class TweenAnimationBuilderScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Tween Animation Builder',
+          ),
+        ),
+        body: Center(
+          child: HSLColorSelector(),
+        ),
+      ),
+    );
+  }
+}
+
+class HSLColorSelector extends StatefulWidget {
+  const HSLColorSelector({Key? key}) : super(key: key);
+
+  @override
+  State<HSLColorSelector> createState() => _HSLColorSelectorState();
+}
+
+class _HSLColorSelectorState extends State<HSLColorSelector> {
+  double _hsl = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        TweenAnimationBuilder<double>(
+          tween: Tween<double>(begin: 0, end: _hsl),
+          duration: const Duration(milliseconds: 1500),
+          builder: (context, hsl, child) {
+            final hslColor = HSVColor.fromAHSV(1, hsl, 1, 1);
+            return Container(
+              width: 200,
+              height: 200,
+              color: hslColor.toColor(),
+            );
+          },
+        ),
+        const SizedBox(
+          height: 48,
+        ),
+        Container(
+          height: 30,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                for (var i = 0; i <= 360; i++)
+                  HSVColor.fromAHSV(1, i.toDouble(), 1, 1).toColor(),
+              ],
+              stops: [
+                for (var i = 0; i <= 360; i++) i.toDouble() / 360,
+              ],
+            ),
+          ),
+        ),
+        Slider.adaptive(
+          value: _hsl,
+          min: 0,
+          max: 360,
+          onChanged: (value) => setState(
+            () {
+              _hsl = value;
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
